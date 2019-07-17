f2ef96883d2742001db265933b5630a1a6467daf
Harmun <hrossouw@student.wethinkcode.co.za>
2019-07-17 16:29:46 +0200

    mvc

---
+++ src/main/java/model/heroes/AHero.java
@@ -0,0 +1,4 @@
+package model.heroes;
+
+public class AHero {
+}
--- src/main/java/view/Startscreen.java
+++ src/main/java/view/Startscreen.java
@@ -4,27 +4,40 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class Startscreen {
+public class Startscreen extends JPanel{
     private JPanel rootPanel;
     private JLabel startLabel;
     private JButton startButton;
     private JTextField nameTextField;
+    private JComboBox startClassListBox;
+    private JPanel startClassPanel;
+    private JTextPane startClassText;
 
     public Startscreen() {
         startButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 String greeting = "Hello " + nameTextField.getText();
                 startLabel.setText(greeting);
-
             }
         });
     }
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("Startscreen");
+        frame.setSize(800,800);
         frame.setContentPane(new Startscreen().rootPanel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
+    }
+
+    private void createUIComponents() {
+        // https://tutorials.tinyappco.com/Java/ComboBoxes
+        // TODO: iterate through classes and add them + change on selection
+        startClassListBox = new JComboBox();
+        startClassListBox.addItem("Warrior");
+        startClassListBox.addItem("Rogue");
+        startClassListBox.addItem("Cleric");
+        startClassListBox.addItem("Wizard");
     }
 }
