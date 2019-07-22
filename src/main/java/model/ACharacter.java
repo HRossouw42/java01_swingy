4d0830c412f91e2584c99e75b44b5a579a0ef451
Harmun <hrossouw@student.wethinkcode.co.za>
2019-07-22 13:07:23 +0200

    abstact doodad and character

---
+++ src/main/java/model/ACharacter.java
@@ -0,0 +1,91 @@
+package model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+
+public abstract class ACharacter extends ADoodad {
+    //instance
+    protected int level = 0;
+
+    //in game
+    protected int hp = 0;
+    protected int maxHp = 0;
+    protected int xp = 0;
+    protected int attack = 0;
+    protected int defence = 0;
+    protected int speed = 0;
+
+
+    protected ACharacter(String name, int level, int coordX, int coordY) {
+        super(name, coordX, coordY);
+        levelUp(level);
+    }
+
+
+    public abstract int getBaseHp();
+
+    public abstract int getBaseXp();
+
+    public abstract int getBaseAttack();
+
+    public abstract int getBaseDefence();
+
+    public abstract int getBaseSpeed();
+
+    public abstract int getBaseGrowthHp();
+
+    public abstract int getBaseGrowthXp();
+
+    public abstract int getBaseGrowthAttack();
+
+    public abstract int getBaseGrowthDefence();
+
+    public abstract int getBaseGrowthSpeed();
+
+
+    protected void levelUp(int newLevel) {
+        if (newLevel <= 0)
+            return;
+
+        // reset to max hp
+        hp = maxHp;
+        maxHp = getBaseHp() + getBaseGrowthHp() * newLevel;
+
+        xp = getBaseXp() + getBaseGrowthXp() * newLevel;
+        attack = getBaseAttack() + getBaseGrowthAttack() * newLevel;
+        defence = getBaseDefence() + getBaseGrowthDefence() * newLevel;
+        speed = getBaseSpeed() + getBaseGrowthSpeed() * newLevel;
+
+        level = newLevel;
+    }
+
+    // public version of function
+    public void levelUp() {
+        levelUp(level + 1);
+    }
+
+    public void setPosition(int coordX, int coordY){
+        this.coordX = coordX;
+        this.coordY = coordY;
+    }
+
+    public void fullHeal(){
+        hp = maxHp;
+    }
+
+    public void getDamaged(int damage) {
+        if (damage < 0)
+            return;
+
+        this.hp = this.hp - damage;
+        // making sure you don't go into minuses when killed
+        if (this.hp < 0)
+            this.hp = 0;
+    }
+
+    //TODO moving
+
+}
---
+++ src/main/java/model/ADoodad.java
@@ -0,0 +1,41 @@
+package model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+// first lombok, generates getter and setter methods
+@Getter @Setter
+
+/*
+abstract keyword is used to create a abstract class and method.
+Abstract class in java can’t be instantiated.
+An abstract class is mostly used to provide a base for subclasses to extend
+    and implement the abstract methods and override or use the implemented methods in abstract class.
+ */
+public abstract class ADoodad {
+    long id = 0;
+
+    // does image fill whole block
+    public boolean fullSize = true;
+
+    // protected is a version of public restricted only to subclasses
+    protected String name = "";
+    protected int coordX = 0;
+    protected int coordY = 0;
+
+    protected ADoodad(String name, int coordX, int coordY) {
+        this.name = name;
+        this.coordX = coordX;
+        this.coordY = coordY;
+
+        //make random ID
+        this.id = getUniqueId();
+    }
+
+    long getUniqueId() {
+        return id ++;
+    }
+
+    //TODO Gui variables and getters
+
+}
